f730d929752e947d15b56125db8e2a909fd15485
yuzhimou <yuzhimou@bytedance.com>
2020-07-03 20:55:38 +0800

    Create yzm.rb

---
+++ Formula/yzm.rb
@@ -0,0 +1,32 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Yzm < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/yourzeromax/homebrew-yzm/blob/master/yzm_0.0.1.tar.gz"
+  sha256 "7c819036222ca94f4f6b8b74cb1f15894d596f87d2f4cf8e882ad728fcc5ce78"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    bin.install "yzm"
+    # system "cmake", ".", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test yzm`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
